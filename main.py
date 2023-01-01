2fe3f9181d1691681918bb1994ba8661934fe862
roy <roy.wood@gmail.com>
2023-01-01 14:46:23 -0500

    Plpay with colours of column headers

--- main.py
+++ main.py
@@ -258,9 +258,9 @@ class MyMenu(curses_gui.MainMenu):
             imdb_search_response = imdb_utils.get_imdb_search_results(video_file.scrubbed_file_name, video_file.year)
         imdb_info_list = imdb_utils.parse_imdb_search_results(imdb_search_response)
 
-        header_columns = [curses_gui.Column('IMDB REFNUM', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_RED),
-                          curses_gui.Column('IMDB NAME', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_RED),
-                          curses_gui.Column('IMDB YEAR', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_RED)]
+        header_columns = [curses_gui.Column('IMDB REFNUM', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_WHITE),
+                          curses_gui.Column('IMDB NAME', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_WHITE),
+                          curses_gui.Column('IMDB YEAR', colour=curses_gui.CursesColourBinding.COLOUR_BLACK_WHITE)]
         header_row = curses_gui.Row(header_columns)
         display_lines = [curses_gui.Row([imdb_info.imdb_tt, imdb_info.imdb_name, imdb_info.imdb_year]) for imdb_info in imdb_info_list]
         with curses_gui.ScrollingPanel(rows=display_lines, header_row=header_row, grid_mode=True, inner_padding=True) as imdb_search_results_panel:
