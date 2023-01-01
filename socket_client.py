a965c664264620d2dab3d6e50e83205472b53e78
roy <roy.wood@gmail.com>
2023-01-01 12:35:41 -0500

    Use runtime arg for IP

--- socket_client.py
+++ socket_client.py
@@ -3,69 +3,82 @@
 
 import socket
 import selectors
+import sys
+
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.setblocking(False)
 
-try:
-    print(f'Connecting to server {HOST}....')
-    client_socket.connect((HOST, PORT))
-except BlockingIOError as e:
-    print('Caught BlockingIOError')
-
-sel = selectors.DefaultSelector()
-sel.register(client_socket, selectors.EVENT_WRITE, 'SOCKET')
-
-client_socket_ready_to_write = False
-client_socket_ready_to_read = False
-
-while not client_socket_ready_to_write:
-    print(f'Waiting for socket read/write readiness....')
-    for selector_key, event_mask in sel.select(0.5):
-        if event_mask & selectors.EVENT_WRITE:
-            print(f'Socket is ready to write')
-            client_socket_ready_to_write = True
-
-msg = 'Socket client says hello....\n'
-byte_count = len(msg)
-print(f'Sending {byte_count} bytes to server')
-byte_count = client_socket.send(bytes(msg, 'UTF-8'))
-print(f'Sent {byte_count} bytes to server')
-
-sel.modify(client_socket, selectors.EVENT_READ, 'SOCKET')
-
-msg_buffer = ''
-keep_going = True
-
-while keep_going:
-    if client_socket_ready_to_read:
-        try:
-            # print(f'Reading bytes from server...')
-            server_bytes = client_socket.recv(8)
-            server_str = server_bytes.decode('utf8')
-            # print(f'Read {len(server_bytes)} bytes from server')
-            msg_buffer += server_str
-            while '\n' in msg_buffer:
-                linefeed_index = msg_buffer.index('\n')
-                server_msg = msg_buffer[:linefeed_index]
-                print(f'Received message: {server_msg}')
-                msg_buffer = msg_buffer[linefeed_index + 1:]
-                if server_msg == 'QUIT':
-                    keep_going = False
-        except BlockingIOError:
-            client_socket_ready_to_read = False
+def main(argv):
+    if argv:
+        host_ip = argv[0]
     else:
+        host_ip = HOST
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.setblocking(False)
+
+    try:
+        print(f'Connecting to server {HOST}....')
+        client_socket.connect((host_ip, PORT))
+    except BlockingIOError as e:
+        print('Caught BlockingIOError')
+
+    sel = selectors.DefaultSelector()
+    sel.register(client_socket, selectors.EVENT_WRITE, 'SOCKET')
+
+    client_socket_ready_to_write = False
+    client_socket_ready_to_read = False
+
+    while not client_socket_ready_to_write:
         print(f'Waiting for socket read/write readiness....')
-        events = sel.select(0.5)
-        for selector_key, event_mask in events:
-            if event_mask & selectors.EVENT_READ:
-                print(f'Socket is ready to read')
-                client_socket_ready_to_read = True
+        for selector_key, event_mask in sel.select(0.5):
+            if event_mask & selectors.EVENT_WRITE:
+                print(f'Socket is ready to write')
+                client_socket_ready_to_write = True
 
-sel.unregister(client_socket)
-sel.close()
+    msg = 'Socket client says hello....\n'
+    byte_count = len(msg)
+    print(f'Sending {byte_count} bytes to server')
+    byte_count = client_socket.send(bytes(msg, 'UTF-8'))
+    print(f'Sent {byte_count} bytes to server')
 
-client_socket.close()
+    sel.modify(client_socket, selectors.EVENT_READ, 'SOCKET')
+
+    msg_buffer = ''
+    keep_going = True
+
+    while keep_going:
+        if client_socket_ready_to_read:
+            try:
+                # print(f'Reading bytes from server...')
+                server_bytes = client_socket.recv(8)
+                server_str = server_bytes.decode('utf8')
+                # print(f'Read {len(server_bytes)} bytes from server')
+                msg_buffer += server_str
+                while '\n' in msg_buffer:
+                    linefeed_index = msg_buffer.index('\n')
+                    server_msg = msg_buffer[:linefeed_index]
+                    print(f'Received message: {server_msg}')
+                    msg_buffer = msg_buffer[linefeed_index + 1:]
+                    if server_msg == 'QUIT':
+                        keep_going = False
+            except BlockingIOError:
+                client_socket_ready_to_read = False
+        else:
+            print(f'Waiting for socket read/write readiness....')
+            events = sel.select(0.5)
+            for selector_key, event_mask in events:
+                if event_mask & selectors.EVENT_READ:
+                    print(f'Socket is ready to read')
+                    client_socket_ready_to_read = True
+
+    sel.unregister(client_socket)
+    sel.close()
+
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- socket_server.py
+++ socket_server.py
@@ -3,73 +3,84 @@
 
 import socket
 import select
+import sys
 import time
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_listen_socket:
-    server_listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_listen_socket.bind((HOST, PORT))
+def main(argv):
+    if argv:
+        host_ip = argv[0]
+    else:
+        host_ip = HOST
 
-    print(f'Configuring listen socket as non-blocking...')
-    server_listen_socket.setblocking(False)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_listen_socket:
+        server_listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_listen_socket.bind((host_ip, PORT))
 
-    print(f'Listening on socket...')
-    server_listen_socket.listen()
+        print(f'Configuring listen socket as non-blocking...')
+        server_listen_socket.setblocking(False)
 
-    while True:
+        print(f'Listening on socket...')
+        server_listen_socket.listen()
+
         while True:
-            print(f'Calling select on listen socket...')
-            readable, writeable, exceptable = select.select([server_listen_socket], [server_listen_socket], [], 1.0)
-            if readable or writeable:
-                break
+            while True:
+                print(f'Calling select on listen socket...')
+                readable, writeable, exceptable = select.select([server_listen_socket], [server_listen_socket], [], 1.0)
+                if readable or writeable:
+                    break
 
-        # print(f'Sleeping before accepting connection...')
-        # time.sleep(5)
+            # print(f'Sleeping before accepting connection...')
+            # time.sleep(5)
 
-        print(f'\n\n')
-        print(f'Accepting connection...')
-        conn, addr = server_listen_socket.accept()
-        conn.setblocking(False)
-        print(f'Connected by {addr}')
+            print(f'\n\n')
+            print(f'Accepting connection...')
+            conn, addr = server_listen_socket.accept()
+            conn.setblocking(False)
+            print(f'Connected by {addr}')
 
-        try:
-            print(f'Servicing connection...')
-            with conn:
-                server_msg_count = 0
-                client_msg_buffer = ''
+            try:
+                print(f'Servicing connection...')
+                with conn:
+                    server_msg_count = 0
+                    client_msg_buffer = ''
 
-                while True:
-                    print(f'Calling select...')
-                    readable, writeable, exceptable = select.select([conn], [conn], [], 5.0)
+                    while True:
+                        print(f'Calling select...')
+                        readable, writeable, exceptable = select.select([conn], [conn], [], 5.0)
 
-                    if readable:
-                        try:
-                            print(f'Socket is readable; reading data...')
-                            client_bytes = conn.recv(1024)
+                        if readable:
+                            try:
+                                print(f'Socket is readable; reading data...')
+                                client_bytes = conn.recv(1024)
 
-                            if not client_bytes:
-                                print(f'Read no data; connection closed; exiting...')
-                                break
+                                if not client_bytes:
+                                    print(f'Read no data; connection closed; exiting...')
+                                    break
 
-                            client_str = client_bytes.decode('utf8')
-                            client_msg_buffer += client_str
-                            while '\n' in client_msg_buffer:
-                                linefeed_index = client_msg_buffer.index('\n')
-                                client_msg = client_msg_buffer[:linefeed_index]
-                                print(f'Received message: {client_msg}')
-                                client_msg_buffer = client_msg_buffer[linefeed_index + 1:]
-                        except BlockingIOError:
-                            print(f'Caught BlockingIOError')
+                                client_str = client_bytes.decode('utf8')
+                                client_msg_buffer += client_str
+                                while '\n' in client_msg_buffer:
+                                    linefeed_index = client_msg_buffer.index('\n')
+                                    client_msg = client_msg_buffer[:linefeed_index]
+                                    print(f'Received message: {client_msg}')
+                                    client_msg_buffer = client_msg_buffer[linefeed_index + 1:]
+                            except BlockingIOError:
+                                print(f'Caught BlockingIOError')
 
-                    if writeable:
-                        print(f'Writing msg #{server_msg_count}')
-                        msg_bytes = f'{server_msg_count=}\n'.encode('utf8')
-                        conn.sendall(msg_bytes)
-                        server_msg_count += 1
-                        time.sleep(3.0)
+                        if writeable:
+                            print(f'Writing msg #{server_msg_count}')
+                            msg_bytes = f'{server_msg_count=}\n'.encode('utf8')
+                            conn.sendall(msg_bytes)
+                            server_msg_count += 1
+                            time.sleep(3.0)
 
-        except BrokenPipeError:
-            print(f'Caught BrokenPipeError; exiting...')
+            except BrokenPipeError:
+                print(f'Caught BrokenPipeError; exiting...')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
